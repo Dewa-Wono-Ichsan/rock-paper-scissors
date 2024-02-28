45eaf8f596da11dd0f77c41c3a1966af5ef13112
Dewa Wono Ichsan <dewa.wono.ichsan@gmail.com>
2024-02-28 21:44:16 +0700

    Identificate problem
    Understand the problem of rock, paper, scissors
    
    Plan input and output of the rock, paper, scissors

---
+++ index.js
@@ -0,0 +1,4 @@
+// Make simple game rock, paper, scissors with javascript
+// Input come from user and computer
+// Participant from user and computer
+// Output the result of the winner in console browser
