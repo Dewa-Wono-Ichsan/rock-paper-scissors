8d23f742229fb99c30995c250904cb90175dfd97
Dewa Wono Ichsan <dewa.wono.ichsan@gmail.com>
2024-02-29 17:06:08 +0700

    Pseudocode
    Plan process algorithm of game rock, paper, scissors using pseudocode

--- index.js
+++ index.js
@@ -2,3 +2,12 @@
 // Input come from user and computer
 // Participant from user and computer
 // Output the result of the winner in console browser
+
+
+// Plan process rock, paper, scissors
+
+// Make variable to store the choice for user
+// Make variable to store the choice for computer
+// Make computer choice between rock, paper, and scissors
+// Make condition of win, draw, and lose the game
+// Print output of the result in console browser
