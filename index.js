4550610f6efe4bc2b55de4b036ecdadfc3250d12
Dewa Wono Ichsan <dewa.wono.ichsan@gmail.com>
2024-02-29 17:35:04 +0700

    Create decision for computer
    Decision of computer to choose between rock, paper, or scissors by using Math.random()
    
    The possiblity divided between numbers between 0 and 10

--- index.js
+++ index.js
@@ -9,5 +9,21 @@
 // Make variable to store the choice for user
 // Make variable to store the choice for computer
 // Make computer choice between rock, paper, and scissors
+    function getComputerChoice() {
+        let computerChoice = Math.random() * 10;
+
+        if (computerChoice < 4) {
+            return "Rock!";
+        } else if (computerChoice < 7) {
+            return "Paper!";
+        } else if (computerChoice < 10) {
+            return "Scissors!";
+        } else {
+            return ""
+        }
+    }
+
+console.log(getComputerChoice());
 // Make condition of win, draw, and lose the game
+// Make battle of rock, paper, scissors between player and computer
 // Print output of the result in console browser
