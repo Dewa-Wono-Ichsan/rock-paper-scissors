f31cf42e3d356f3ede29d4233ac2d6dd5864acf3
Dewa Wono Ichsan <dewa.wono.ichsan@gmail.com>
2024-02-29 19:05:49 +0700

    single round rock, paper, scissors
    Found wrong result while testing the conditional statement with console.log()

--- index.js
+++ index.js
@@ -29,21 +29,30 @@
         if ( 
             (playerSelection === "rock" || "Rock" || "ROCK") 
             && ComputerSelection === "Scissors!") {
-                return "You win! Rock defeat scissors";
+                return "You win! Rock defeat scissors"; //win condition
         } else if ( 
             (playerSelection === "paper" || "Paper" || "PAPER") 
             && ComputerSelection === "Rock!") {
-                return "You win! Paper defeat scissors";
+                return "You win! Paper defeat rock"; //win condition
         } else if ( 
             (playerSelection === "scissors" || "Scissors" || "SCISSORS")
             && ComputerSelection === "Paper!") {
-                return "You win! Scissors defeat paper"
+                return "You win! Scissors defeat paper"; //win condition
+        } else if (
+            (playerSelection === "rock" || "Rock" || "ROCK")
+            && ComputerSelection === "Rock!"
+        ) {
+                return "You draw! Battle between rock power"
         }
         else {
             return "please choose between rock, paper, or scissors"
         }
     }
     console.log(singleRound("scissors", "Paper!"));
+    console.log(singleRound("rock", "Rock!")); //wrong result
+    console.log(singleRound("sss", "Paper!")); //wrong result
+    console.log(singleRound("rrr","Rock!")); //wrong result
+    console.log(singleRound("qqq", "Scissors!"));//wrong result
 
 // Make battle of rock, paper, scissors between player and computer
 // Print output of the result in console browser
