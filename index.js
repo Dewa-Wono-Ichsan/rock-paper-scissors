f37b357c0bf8b03e7bb32d64ed442a7f478c461f
Dewa Wono Ichsan <dewa.wono.ichsan@gmail.com>
2024-03-02 20:46:44 +0700

    Edit singleRound function
    Add variable with name singleRoundResult to contain the return value of singleRound function for later conditional statement

--- index.js
+++ index.js
@@ -26,63 +26,65 @@
 
 // Make condition of win, draw, and lose the game
     function singleRound(playerSelection, ComputerSelection) {
+
+        let singleRoundResult;
         if (playerSelection === "rock" && ComputerSelection === "Scissors!"
             || playerSelection === "Rock" && ComputerSelection === "Scissors!"
             || playerSelection === "ROCK" && ComputerSelection === "Scissors!"
             || playerSelection === "RocK" && ComputerSelection === "Scissors!"
             || playerSelection === "RoCK" && ComputerSelection === "Scissors!") {
-            return "You Win! Rock defeat scissors"
+            return singleRoundResult = "You Win! Rock defeat scissors"
         } else if (playerSelection === "paper" && ComputerSelection === "Rock!"
         || playerSelection === "Paper" && ComputerSelection === "Rock!"
         || playerSelection === "PAPER" && ComputerSelection === "Rock!"
         || playerSelection === "PapeR" && ComputerSelection === "Rock!"
         || playerSelection === "PAper" && ComputerSelection === "Rock!") {
-        return "You Win! Paper defeat rock"
+        return singleRoundResult = "You Win! Paper defeat rock"
         } else if (playerSelection === "scissors" && ComputerSelection === "Paper!"
         || playerSelection === "Scissors" && ComputerSelection === "Paper!"
         || playerSelection === "SCISSORS" && ComputerSelection === "Paper!"
         || playerSelection === "ScissorS" && ComputerSelection === "Paper!"
         || playerSelection === "sCISSORs" && ComputerSelection === "Paper!") {
-        return "You Win! Scissors defeat paper" // three win condition
+        return singleRoundResult = "You Win! Scissors defeat paper" // three win condition
         } else if (playerSelection === "rock" && ComputerSelection === "Rock!"
         || playerSelection === "Rock" && ComputerSelection === "Rock!"
         || playerSelection === "ROCK" && ComputerSelection === "Rock!"
         || playerSelection === "ROck" && ComputerSelection === "Rock!"
         || playerSelection === "roCK" && ComputerSelection === "Rock!") {
-        return "You draw! Rock has same power with rock"
+        return singleRoundResult = "You draw! Rock has same power with rock"
         } else if (playerSelection === "paper" && ComputerSelection === "Paper!"
         || playerSelection === "Paper" && ComputerSelection === "Paper!"
         || playerSelection === "PAPER" && ComputerSelection === "Paper!"
         || playerSelection === "PAper" && ComputerSelection === "Paper!"
         || playerSelection === "papER" && ComputerSelection === "Paper!") {
-        return "You draw! Paper has same power with paper"
+        return singleRoundResult = "You draw! Paper has same power with paper"
         } else if (playerSelection === "scissors" && ComputerSelection === "Scissors!"
         || playerSelection === "Scissors" && ComputerSelection === "Scissors!"
         || playerSelection === "SCISSORS" && ComputerSelection === "Scissors!"
         || playerSelection === "SCIssors" && ComputerSelection === "Scissors!"
         || playerSelection === "sciSSORS" && ComputerSelection === "Scissors!") {
-        return "You draw! Scissors has same power with scissors" // three draw condition
+        return singleRoundResult = "You draw! Scissors has same power with scissors" // three draw condition
         } else if (playerSelection === "rock" && ComputerSelection === "Paper!"
         || playerSelection === "Rock" && ComputerSelection === "Paper!"
         || playerSelection === "ROCK" && ComputerSelection === "Paper!"
         || playerSelection === "ROck" && ComputerSelection === "Paper!"
         || playerSelection === "roCK" && ComputerSelection === "Paper!") {
-        return "You lose! Rock cannot defeat paper"
+        return singleRoundResult = "You lose! Rock cannot defeat paper"
         } else if (playerSelection === "paper" && ComputerSelection === "Scissors!"
         || playerSelection === "Paper" && ComputerSelection === "Scissors!"
         || playerSelection === "PAPER" && ComputerSelection === "Scissors!"
         || playerSelection === "PAPer" && ComputerSelection === "Scissors!"
         || playerSelection === "papER" && ComputerSelection === "Scissors!") {
-        return "You lose! Paper has no power against scissors"
+        return singleRoundResult = "You lose! Paper has no power against scissors"
         } else if (playerSelection === "scissors" && ComputerSelection === "Rock!"
         || playerSelection === "Scissors" && ComputerSelection === "Rock!"
         || playerSelection === "SCISSORS" && ComputerSelection === "Rock!"
         || playerSelection === "SCIssors" && ComputerSelection === "Rock!"
         || playerSelection === "sciSSORS" && ComputerSelection === "Rock!") {
-        return "You lose! Scissors must be faced with its greatest fear" //three lose condition
+        return singleRoundResult ="You lose! Scissors must be faced with its greatest fear" //three lose condition
         }
         else {
-            return "try something else"
+            return singleRoundResult = "try something else"
         }
     }
 
@@ -91,12 +93,8 @@
         // each round getting score for win, draw,or lose
     //Make decision for player in win, draw, or lose rock, paper, scissors
 function playGame() {
-    return singleRound(prompt("Decide your choice between rock, paper, and scissors"), getComputerChoice());
+    return singleRound(prompt("Decide your choice between rock, paper, and scissors"), getComputerChoice());   
 }
 
-console.log(playGame());
-console.log(playGame());
-console.log(playGame());
-console.log(playGame());
 console.log(playGame());
 // Print output of the result in console browser
