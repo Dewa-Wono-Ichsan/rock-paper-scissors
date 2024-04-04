63d6132c0e85c4fd1877728f2eee2daadd462bcb
Dewa Wono Ichsan <dewa.wono.ichsan@gmail.com>
2024-04-04 21:57:03 +0700

    Set Match and set condition for get point
    Create variable score to store total point in every round, increment score when win against computer, decrement score when lose against computer, keep same score when draw.
    
    Create variable playerSelection to store player choice and variable computerSelection to store data from function getComputerChoice().
    
    Set condition for win, lose, and draw every time user click the button with id rock.

--- index.js
+++ index.js
@@ -19,11 +19,31 @@ const rock = document.querySelector('#rock')
 const paper = document.querySelector('#paper')
 const scissors = document.querySelector('#scissors')
 
+let score = 0
+
 rock.addEventListener('click', () => {
-console.log('rock')})
+    let playerSelection
+    let computerSelection
+    console.log(playerSelection = 'rock')
+    console.log(computerSelection = getComputerChoice())
+    if (playerSelection === 'rock' && computerSelection === 'Rock!') {
+    console.log(score++)
+        }
+    else if (playerSelection === 'rock' && computerSelection === 'Paper!') {
+    console.log(score--)
+        }
+    else {
+    console.log(score)
+        }
+    }
+)
 
 paper.addEventListener('click', () => { 
-console.log('paper')})
+    console.log('paper')
+    }
+)
 
 scissors.addEventListener('click', () => { 
-console.log('scissors')})
+    console.log('scissors')
+    }
+)
