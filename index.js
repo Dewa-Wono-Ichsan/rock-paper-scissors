fe5f2a37f0491dc47c8b70a1c94361a5fe14c6e8
Dewa Wono Ichsan <dewa.wono.ichsan@gmail.com>
2024-04-05 13:40:51 +0700

    Create function in eventListener paper button
    Add same feature in paper button as rock button.
    
    Change input from user in paper button that store user selection for paper.
    
    Display result in DOM for player selection, score, and result of game rock, paper, scissors.

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ rock.addEventListener('click', () => {
     let playerSelection = 'rock'
     let computerSelection = getComputerChoice()
     const result = document.createElement('div')
-    
+
         if (score !== 5 && score !== -5) {
             const div = document.createElement('div')
             document.body.appendChild(div)
@@ -74,11 +74,57 @@ rock.addEventListener('click', () => {
 )
 
 paper.addEventListener('click', () => { 
-    console.log('paper')
+    let playerSelection = 'paper'
+    let computerSelection = getComputerChoice()
+    const result = document.createElement('div')
+
+        if (score !== 5 && score !== -5) {
+            const div = document.createElement('div')
+            document.body.appendChild(div)
+            div.classList.toggle('player-choice')
+            div.textContent = 
+            `user choose ${playerSelection} and computer choose ${computerSelection}`
+            
+            const point = document.createElement('div')
+            document.body.appendChild(point)
+            point.classList.toggle('point')
+
+
+            if (playerSelection === 'paper'
+                && computerSelection === 'Rock!') {
+                
+                point.textContent = score++
+              }
+            else if (playerSelection === 'paper' 
+                && computerSelection === 'Scissors!') {
+                
+
+                point.textContent = score--
+                }
+            else {
+                point.textContent = score
+                }
+        } else if (score === 5) {
+            
+            document.body.appendChild(result)
+            result.classList.toggle('result')
+            result.textContent =  'user win'
+            rock.remove()
+            paper.remove()
+            scissors.remove()
+        } else {
+            
+            document.body.appendChild(result)
+            result.classList.toggle('result')
+            result.textContent = 'computer win'
+            rock.remove()
+            paper.remove()
+            scissors.remove()
+        }
     }
 )
 
 scissors.addEventListener('click', () => { 
-    console.log('scissors')
+
     }
 )
